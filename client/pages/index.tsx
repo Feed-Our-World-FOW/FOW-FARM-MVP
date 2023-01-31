af1620ec395c2ca8af85347ad3e64527b22f9fd6
Ankush263 <ankushbanik263@gmail.com>
2023-01-31 16:19:56 +0530

    update

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import SignInPage from './components/SignInPage'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <SignInPage />
+    </div>
+  )
+}
+
+export default Home
