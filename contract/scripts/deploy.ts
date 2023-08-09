dfcec463fbd5f8375e56aea87a596980678bc8f0
Ankush263 <ankushbanik263@gmail.com>
2023-08-09 16:18:56 +0530

    update

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,19 +1,18 @@
-import ethers from "@nomiclabs/hardhat-ethers"
-const hre = require("hardhat")
+import ethers from '@nomiclabs/hardhat-ethers';
+const hre = require('hardhat');
 
 async function main() {
+	const FOW_transaction = await hre.ethers.getContractFactory(
+		'FOW_transaction'
+	);
+	const fow_transaction = await FOW_transaction.deploy();
 
-  const FOW_transaction = await hre.ethers.getContractFactory("FOW_transaction")
-  const fow_transaction = await FOW_transaction.deploy()
+	await fow_transaction.deployed();
 
-  await fow_transaction.deployed()
-
-  console.log(`FOW_transaction deployed to ${fow_transaction.address}`)
-
+	console.log(`FOW_transaction deployed to ${fow_transaction.address}`);
 }
 
-
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
